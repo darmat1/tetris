0b4a2719c37423100afb1755bafd24c4dd0a2f35
Andrew Kupriyanov <andrew.darmat@gmail.com>
2021-02-16 04:42:41 +0200

    bin removed

---
+++ Tetris/IDrawer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris
+{
+    interface IDrawer
+    {
+       void DrawPoint(int x, int y);
+       void DrawEmpty(int x, int y);
+    }
+}
